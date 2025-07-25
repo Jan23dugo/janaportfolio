9d74cc54b3feba3301c281c47674503945662660
Jan23dugo <jdugo23@gmail.com>
2025-07-25 19:35:42 +0800

    fdsafasd

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-console.log('REACT_APP_SUPABASE_URL:', process.env.REACT_APP_SUPABASE_URL);
-console.log('REACT_APP_SUPABASE_ANON_KEY:', process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
